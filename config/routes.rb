44c0142fcee472ddd702d7d7c55c07a172723419
Carson Leong <carsonleong0930@hotmail.com>
2016-08-04 09:28:37 +0800

    Day 3 task

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   root to: 'landing#index'
+  get :about, to: 'static_pages#about'
+  resources :topics
   # get :carson, to: 'page#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
