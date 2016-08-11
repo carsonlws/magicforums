6bf309aa8c4f8dcdd7aec41424349f73896dd293
Carson Leong <carsonleong0930@hotmail.com>
2016-08-11 10:25:57 +0800

    syntax fixes and polishing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
     def create
       user = User.find_by(email: user_params[:email])
-                 &.authenticate(user_params[:password])
+              &.authenticate(user_params[:password])
 
       if user
         session[:id] = user.id
---
+++ app/policies/comment_policy.rb
@@ -0,0 +1,29 @@
+class CommentPolicy < ApplicationPolicy
+
+  def new?
+    user.present? && user_has_power?
+  end
+
+  def create?
+    new?
+  end
+
+  def edit?
+    user.present? && record.user == user || user_has_power?
+  end
+
+  def update?
+    edit?
+  end
+
+  def destroy?
+    edit?
+  end
+
+
+private
+
+  def user_has_power?
+    user.admin? || user.moderator?
+  end
+end
